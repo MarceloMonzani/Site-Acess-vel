1ed075fb2fd0fb98dc1fbf438cb0eb752498d2f5
MarceloMonzani <00001124372805SP@al.educacao.sp.gov.br>
2025-09-05 15:52:24 -0300

    Update script.js

--- script.js
+++ script.js
@@ -22,8 +22,9 @@ document.addEventListener('DOMContentLoaded', function () {
  botaoDeAcessibilidade.addEventListener('click', () => {
  botaoDeAcessibilidade.classList.toggle('rotacao-botao');
  opcoesDeAcessibilidade.classList.toggle('apresenta-lista');
+ const botaoSelecionado = botaoDeAcessibilidade.getAttribute('aria-expanded') === 'true';
+ botaoDeAcessibilidade.setAttribute('aria-expanded', !botaoSelecionado);
  })
  alternaContraste.addEventListener('click', function(){
  document.body.classList.toggle('alto-contraste');
- })
  })
